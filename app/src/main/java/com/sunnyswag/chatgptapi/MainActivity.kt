a0d51305177432c2ba122139784c5827fc98aa78
sunnyswag <hhq126152@gmail.com>
2023-03-10 08:20:16 +0800

    add the listener of page changed

--- app/src/main/java/com/sunnyswag/chatgptapi/MainActivity.kt
+++ app/src/main/java/com/sunnyswag/chatgptapi/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sunnyswag.chatgptapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 import com.drakeet.multitype.MultiTypeAdapter
 import com.sunnyswag.chatgptapi.databinding.ActivityMainBinding
 import com.sunnyswag.chatgptapi.uibinder.ChatHistoryUiBinder
@@ -11,7 +12,6 @@ import com.sunnyswag.chatgptapi.uimodel.StartChatUiModel
 
 class MainActivity : AppCompatActivity() {
 
-    private val TAG = "MainActivity"
     private val adapter = MultiTypeAdapter().apply {
         register(StartChatUiModel::class, StartChatUiBinder())
         register(ChatHistoryUiModel::class, ChatHistoryUiBinder())
@@ -24,11 +24,48 @@ class MainActivity : AppCompatActivity() {
             setContentView(it.root)
         }
 
+        initAdapter(binding)
+        initPageSelectedListener(binding)
+    }
+
+    private fun initPageSelectedListener(binding: ActivityMainBinding) {
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_start_chat -> {
+                    binding.vpMain.currentItem = START_CHAT_PAGE
+                    true
+                }
+                R.id.nav_chat_history -> {
+                    binding.vpMain.currentItem = CHAT_HISTORY_PAGE
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.vpMain.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                when (position) {
+                    START_CHAT_PAGE -> binding.bottomNavigationView.selectedItemId =
+                        R.id.nav_start_chat
+                    CHAT_HISTORY_PAGE -> binding.bottomNavigationView.selectedItemId =
+                        R.id.nav_chat_history
+                }
+            }
+        })
+    }
+
+    private fun initAdapter(binding: ActivityMainBinding) {
         binding.vpMain.adapter = adapter
         adapter.items = listOf(
             StartChatUiModel(),
-            ChatHistoryUiModel(),
-            StartChatUiModel()
+            ChatHistoryUiModel()
         )
+    }
+
+    companion object {
+        private const val START_CHAT_PAGE = 0
+        private const val CHAT_HISTORY_PAGE = 1
     }
 }
